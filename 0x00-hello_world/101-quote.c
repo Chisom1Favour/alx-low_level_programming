cd998aa658435169b03613a6f41819526812464b
Chisom Favour <favourchisom578@gmail.com>
2022-06-04 13:33:35 -0700

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: prints a string to the standard error
+ *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
