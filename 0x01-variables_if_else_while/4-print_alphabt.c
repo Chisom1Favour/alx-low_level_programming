eddfa9b549cfe1b9caa7253daea11c348162e3da
Chisom Favour <favourchisom578@gmail.com>
2022-06-06 13:01:42 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ * followed by a new line
+ * except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+	if (ch != 'q' && ch != 'e')
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	}
+
+	return (0);
+}
