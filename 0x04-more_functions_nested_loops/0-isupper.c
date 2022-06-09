edfb5742bb0b431d7cfca1401e9e02767f879dff
Chisom Favour <favourchisom578@gmail.com>
2022-06-09 08:19:46 -0700

    lady

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function that checks for uppercase character
+ *
+ * Return: 1 if c is uppercase
+ *
+ * Return: 0 if otherwise
+ *
+ */
+int main(void)
+{
+	char c;
+
+	if (c >='A' && c <= 'Z')
+	{
+		putchar('c');
+	}
+	return 1
+	else
+	{
+		putchar('c');
+	}
+	return 0
+}
