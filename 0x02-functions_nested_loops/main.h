c9c081d5239e0b96410450df9d297e29618316d6
Chisom Favour <favourchisom578@gmail.com>
2022-06-09 03:08:21 -0700

    hot fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef _main.h_
-#define _main.h_
-
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,6 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
-
