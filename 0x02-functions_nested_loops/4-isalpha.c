31b97b0848612e95fcbef001b474db3b77fccf2f
Chisom Favour <favourchisom578@gmail.com>
2022-06-09 22:52:49 -0700

    new one

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
  * _isalpha - returns 1 if c is a letter, uppercase or lowercase
  * returns 0 if otherwise
  *
- * @c: The character is ASCII code
+ * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 if otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c <= 97 && c >= 122))
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
 		return (1);
 	}
