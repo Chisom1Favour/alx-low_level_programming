17667dc9f2f97b82773df6524cdc63f2da22aa44
Chisom Favour <favourchisom578@gmail.com>
2022-06-09 22:33:33 -0700

    alpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if c is a letter, uppercase or lowercase
+ * returns 0 if otherwise
+ *
+ * @c: The character is ASCII code
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * return 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c <= 97 && c >= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+
+}
+
