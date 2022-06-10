8334c99b168f8d689a2349cbdd259b91b6826cd7
Chisom Favour <favourchisom578@gmail.com>
2022-06-10 05:21:10 -0700

    okayy

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the minutes of a day
+ *
+ * Return: Nothing
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+		for (i = 48; i <= 50; i++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				for (k = 48; k <= 53; k++)
+				{
+					for (l = 48; l <= 57; l++)
+					{
+
+					if  (i >= 50 && j >= 52)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+
+					}
+				}
+			}
+		}
+}
+
+
