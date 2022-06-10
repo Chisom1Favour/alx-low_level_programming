af79a7a92b7844a00f007d1837ea92f4b2e1e7df
Chisom Favour <favourchisom578@gmail.com>
2022-06-09 23:49:01 -0700

    pop

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - shows if n is greater, equal to or less than zero
+ *
+ * @n: n is an integer
+ *
+ * Return: 1 if n is greater than zero. 0 if n is equal to zero.
+ * -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+	return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+	}
+	return (0);
+	else
+	{
+		_putchar(45);
+	}
+	return (-1);
+	_putchar('\n');
+}
