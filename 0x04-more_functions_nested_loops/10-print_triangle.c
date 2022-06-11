f36303f8f1155012ebfe9ddd656fce92e01c6995
Chisom Favour <favourchisom578@gmail.com>
2022-06-10 19:21:14 -0700

    few

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,11 +14,11 @@ void print_triangle(int size)
 	for (x = 0; x < size; x++)
 	{
 		for (y = 1; y < (size - x); y++)
-			_putchar(' ')
+			_putchar(' ');
 		for (y--; y < size; y++)
 			_putchar(35);
 		if (x < (size - 1))
-			_putchar('\n')
+			_putchar('\n');
 	}
 	_putchar('\n');
 }
