35466ef8dfa698bc37ba6001e62486011448b6f4
Chisom Favour <favourchisom578@gmail.com>
2022-06-14 04:43:40 -0700

    tough one

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,11 @@ void rev_string(char *s)
 
 	length1 = 0;
 	length2 = 0;
-	while (s[length1] != '\0')
-		 length1++;
 
-	 length2 = length1 - 1;
+	while (s[length1] != '\0')
+
+	length1++;
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
 
