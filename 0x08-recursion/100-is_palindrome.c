24f624ee8fe25a8d226c04f5803423a16346dbad
Chisom Favour <favourchisom578@gmail.com>
2022-06-23 20:32:10 -0700

    palindrome checker

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - size
+ *
+ * @s: pointer to string parameter
+ *
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
+
+/**
+ * p1 - palindrome
+ *
+ * @s: pointer to string
+ *
+ * @1: position
+ *
+ * Return: boolean
+ */
+
+int p1(char *s, int I)
+{
+	if (I < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + I))
+	{
+		return (p1(s + 1, I - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ *
+ * @s: pointer to string
+ *
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (p1(s, len - 1));
+}
