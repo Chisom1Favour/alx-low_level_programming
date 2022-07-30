65862a9f4ca8af1c3d71396dec54256bbbe64813
Chisom Favour <favourchisom578@gmail.com>
2022-07-29 18:08:42 -0700

    new changes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,10 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 
-	if (fd == 1)
+	if (fd == -1)
 		return (0);
+
 	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
